1a7deb5981e0790f7d9855e1b91cb42214297572
Mahmoud <abderahman.mahmoud@motrac.be>
2019-03-28 21:22:04 +0100

    New-ADUser cmdlet added, needs to be completed

--- NewADUser.ps1
+++ NewADUser.ps1
@@ -2,9 +2,14 @@
 $employee_number = Read-Host 'Employee number: '
 $firstname = Read-Host 'First name: '
 $lastname = Read-Host 'Last name: '
+$displayname = "$lastname, (" + $firstname.Substring(0,1) + ". $firstname)"
 $birthdate = Read-Host 'Birthdate (dd-mm-jjjj): '
 $phone = Read-Host 'Phone number: '
+$password = Read-Host -AsSecureString 'Motrac01';
 $job_title = Read-Host 'Job title: '
+$username = $firstname + "." + $lastname
+$mail = $username + "@motrac.be"
+
 
 $listCompanys = 'OU=Accounts,dc=reesink,dc=nl'
 Write-Host 'Select a company'
@@ -12,5 +17,4 @@ $company = Get-ADOrganizationalUnit -Filter * -SearchBase $listCompanys -SearchS
 $company.DistinguishedName=$company.DistinguishedName.Insert(0,'OU=Gebruikers,')
 $company.Name = 'Gebruikers'
 $department = Get-ADOrganizationalUnit -Filter * -SearchBase $company.DistinguishedName -SearchScope OneLevel | Sort {-join ($_.distinguishedname[($_.distinguishedname.length-1)..0])} | Sort-Object | Select name, DistinguishedName |Out-GridView -Title "Select a department for $firstname" -Passthru #list all department of company
-$location
-$country
+new-ADUser $username -Enabled $true -AccountPassword $password -Path $department.DistinguishedName -Department $department.Name -Description $Description -DisplayName $displayname -Office $Office -Title $Description -OfficePhone $phone -SamAccountName $username -GivenName $firstname -Surname $lastname
